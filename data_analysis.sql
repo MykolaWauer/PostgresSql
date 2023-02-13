eab7bb2b1ce70ad919a840dc31a989fa899f8a8a
MykolaWauer <51316424+MykolaWauer@users.noreply.github.com>
2023-02-13 09:11:38 +0100

    Add files via upload

---
+++ data_analysis.sql
@@ -0,0 +1,222 @@
+-- Get the names and the quantities in stock for each product:
+
+SELECT 
+	product_name AS product_name
+	, quantity_per_unit AS quantity	
+	, units_in_stock AS units_in_stock
+
+FROM 
+ 	products;
+
+-- Get a list of current products (Product ID and name):
+
+SELECT 
+	product_id AS id
+	, product_name AS product_name
+
+FROM 
+	products;
+
+-- Get a list of the most and least expensive products (name and unit price):
+
+SELECT 
+	x.product_name
+	, x.unit_price
+FROM 
+	products x
+JOIN (
+  SELECT y.product_id 
+  FROM products y
+  ORDER BY unit_price DESC
+  LIMIT 5
+) y ON x.product_id = y.product_id
+
+UNION
+
+SELECT 
+	x.product_name
+	, x.unit_price
+FROM 
+	products x
+JOIN (
+  SELECT y.product_id 
+  FROM products y
+  ORDER BY unit_price ASC
+  LIMIT 5
+) y ON x.product_id = y.product_id
+
+ORDER BY unit_price DESC;
+
+
+-- Get products that cost less than $20:
+
+SELECT 
+	product_name
+	, unit_price
+FROM 
+	products
+WHERE 
+	unit_price < 20
+ORDER BY 
+	unit_price DESC;
+
+-- Get products that cost between $15 and $25:
+
+SELECT 
+	product_name
+	, unit_price
+FROM 
+	products
+WHERE 
+	unit_price >= 15 AND unit_price <= 25
+ORDER BY 
+	unit_price DESC
+
+-- Get products above average price:
+
+WITH avg_price AS (
+  SELECT 
+  	AVG(unit_price) AS avg_price
+  FROM 
+  	products
+)
+
+SELECT 
+	product_name
+	, unit_price
+FROM 
+	products p
+JOIN 
+	avg_price a ON p.unit_price > a.avg_price
+ORDER BY 
+	unit_price DESC;
+	
+-- Find the ten most expensive products:
+
+SELECT 
+	product_Name
+	, unit_price
+FROM 
+	products
+ORDER BY 
+	unit_price DESC
+LIMIT 10;
+
+-- Get a list of discontinued products (Product ID and name):
+
+SELECT 
+	product_id
+	, product_name
+FROM 
+	products
+WHERE 
+	discontinued = TRUE;
+	
+-- Count current and discontinued products:
+
+SELECT
+	CASE 
+		WHEN discontinued = FALSE THEN 'current' 
+	ELSE 'discontinued' 
+	END AS Product_Status,
+  	COUNT(*) AS ProductCount
+FROM 
+	products
+GROUP BY 
+	Product_Status;
+	
+-- Find products with less units in stock than the quantity on order:
+
+SELECT 
+	product_id
+	, product_name
+	, units_in_stock
+	, units_on_order
+FROM 
+	products
+WHERE 
+	units_in_stock < units_on_order;
+	
+-- Find the customer who had the highest order amount:
+
+WITH raw_data AS (
+	SELECT 
+		DISTINCT o.customer_id AS customer_id
+		, o.order_id
+		, od.quantity
+	FROM 
+		orders o
+	LEFT JOIN 
+		order_details od ON o.order_id = od.order_id
+	ORDER BY quantity DESC
+	)
+	
+SELECT
+	DISTINCT rd.customer_id AS customer_id,
+	c.company_name AS customer_name,
+	SUM(quantity) AS quantity
+
+FROM 
+	raw_data rd
+LEFT JOIN 
+	customers c ON rd.customer_id = c.customer_id
+GROUP BY 
+	rd.customer_id
+	, customer_name
+ORDER BY 
+	quantity DESC
+LIMIT 1;
+
+-- Get orders for a given employee and the according customer:
+
+WITH raw_data AS (
+	SELECT 
+		DISTINCT o.customer_id AS customer_id
+		, o.order_id AS order_id
+		, o.employee_id AS employee_id
+		, c.company_name AS customer_name
+	FROM 
+		orders o
+	LEFT JOIN 
+		customers c ON o.customer_id = c.customer_id
+		)
+SELECT
+	rd.employee_id AS employee_id
+	, CONCAT(c.titleofcourtesy, ' ', c.first_name, ' ', c.last_name) AS name --concat  
+	, rd.customer_id AS customer_id
+	, rd.customer_name AS customer_name
+	, rd.order_id AS order_id
+	
+FROM 
+	raw_data rd
+LEFT JOIN 
+	employees c ON rd.employee_id = c.employee_id
+ORDER BY 
+	rd.employee_id ASC;
+	
+
+-- Find the hiring age of each employee:
+
+SELECT 
+	employee_id
+	, CONCAT(titleofcourtesy, ' ', first_name, ' ', last_name) AS name
+	, EXTRACT(YEAR FROM hire_Date) - EXTRACT(YEAR FROM birth_Date) AS hire_age
+
+	
+FROM 
+	employees
+ORDER BY hire_age DESC;
+
+-- What is the total revenue delivered in each country? Named Query:
+CREATE VIEW TotalRevenueByCountry AS
+SELECT 
+  c.country AS country,
+  SUM(od.unit_price * od.quantity *(1-discount)) AS revenue
+FROM 
+  order_details od
+  INNER JOIN orders o ON od.order_id = o.order_id
+  INNER JOIN customers c ON o.customer_id = c.customer_id
+GROUP BY 
+  c.country
+ORDER BY 
+  revenue DESC
---
+++ data_model_tahinis.sql
@@ -0,0 +1,215 @@
+drop table if EXISTS categories cascade;
+CREATE table categories (
+    category_ID SERIAL PRIMARY KEY,
+    category_name VARCHAR(25),
+    description VARCHAR,
+    picture BYTEA
+);
+
+\copy categories FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/categories.csv' DELIMITER ',' CSV HEADER;
+
+
+-- ***********************************************************************************************************************************
+--                                  CUSTOMERS
+-- ***********************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS customers CASCADE;
+-- Creating customers table --
+CREATE TABLE customers (
+    customer_ID VARCHAR(20) PRIMARY KEY,
+    company_Name VARCHAR(150),
+    contact_Name VARCHAR(200),
+    contact_Title VARCHAR(100),
+    address VARCHAR(255),
+    city VARCHAR(50),
+    region VARCHAR(50),
+    postal_Code VARCHAR(50),
+    country VARCHAR(20),
+    phone VARCHAR(25),
+    fax VARCHAR(25)
+);
+-- Inserting Data --
+\copy customers FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/customers.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     EMPLOYEES
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS employees CASCADE;
+-- Creating employees table --
+CREATE TABLE employees (
+    employee_ID INT PRIMARY KEY,
+    last_Name VARCHAR(50),
+    first_Name VARCHAR(50),
+    title VARCHAR(100),
+    titleOfCourtesy VARCHAR(15),
+    birth_Date DATE,
+    hire_Date DATE,
+    address VARCHAR(150),
+    city VARCHAR(20),
+    region VARCHAR(20),
+    postal_Code VARCHAR(20),
+    country VARCHAR(20),
+    home_Phone VARCHAR(25),
+    extension VARCHAR(15),
+    photo TEXT,
+    notes VARCHAR(255),
+    reports_To INT, 
+    photo_Path VARCHAR(200)
+   
+);
+-- Inserting Data --
+\copy employees FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/employees.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     SHIPPERS
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS shippers CASCADE;
+-- Creating shippers table --
+CREATE TABLE shippers (
+    shipper_ID SERIAL PRIMARY KEY,
+    company_Name VARCHAR(50) NOT NULL,
+    phone VARCHAR(20)
+);
+-- Inserting Data --
+\copy shippers FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/shippers.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     ORDERS
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS orders CASCADE;
+-- Creating orders table --
+CREATE TABLE orders (
+    order_ID SERIAL PRIMARY KEY,
+    customer_ID VARCHAR(20) REFERENCES customers (customer_ID),
+    employee_ID INT REFERENCES employees (employee_ID),
+    order_Date DATE,
+    required_Date DATE,
+    shipped_Date DATE,
+    ship_Via INT REFERENCES shippers (shipper_ID),
+    freight FLOAT,
+    ship_Name VARCHAR(50),
+    ship_Address VARCHAR(50),
+    ship_City VARCHAR(50),
+    ship_Region VARCHAR(50),
+    ship_Postal_Code VARCHAR(20),
+    ship_Country VARCHAR(20)
+);
+-- Inserting Data --
+\copy orders FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/orders.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     REGIONS
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS regions CASCADE;
+-- Creating orders table --
+CREATE TABLE regions (
+    region_ID SERIAL PRIMARY KEY,
+    region_Description VARCHAR(20)
+    
+);
+-- Inserting Data --
+\copy regions FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/regions.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     SUPPLIERS
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS suppliers CASCADE;
+-- Creating orders table --
+CREATE TABLE suppliers (
+    supplier_ID	SERIAL PRIMARY KEY,
+	company_Name VARCHAR(50),
+	contact_Name VARCHAR(50),
+	contact_Title VARCHAR(50),
+	address	VARCHAR(50),
+	city VARCHAR(20),
+	region VARCHAR(255),
+	postal_Code VARCHAR(20),
+	country	VARCHAR(20),
+	phone VARCHAR(20),
+	fax	VARCHAR(20),
+	home_Page VARCHAR(250)
+    
+);
+-- Inserting Data --
+\copy suppliers FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/suppliers.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     PRODUCTS
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS products CASCADE;
+-- Creating orders table --
+CREATE TABLE products (
+    product_ID SERIAL  PRIMARY KEY,
+	product_Name VARCHAR(255),
+	supplier_ID SERIAL REFERENCES suppliers (supplier_ID),
+	category_ID SERIAL REFERENCES categories (category_ID),
+	quantity_Per_Unit VARCHAR(255),
+	unit_Price DECIMAL,
+	units_In_Stock INTEGER,
+	units_On_Order INTEGER,
+	reorder_Level INTEGER,
+	discontinued BOOL
+    
+);
+-- Inserting Data --
+\copy products FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/products.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     Territories
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS territories CASCADE;
+-- Creating orders table --
+CREATE TABLE territories (
+    territory_ID INTEGER PRIMARY KEY,
+    territory_Description VARCHAR(255),	
+    region_ID SERIAL REFERENCES regions (region_ID)
+	    
+);
+-- Inserting Data --
+\copy territories FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/territories.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     Employee Territories
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS employee_territories CASCADE;
+-- Creating orders table --
+CREATE TABLE employee_territories (
+	employee_ID	INTEGER REFERENCES employees (employee_ID),
+	territory_ID INTEGER REFERENCES territories (territory_ID)
+	 
+);
+-- Inserting Data --
+\copy employee_territories FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/employee_territories.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+-- *************************************************************************************************************************************
+--                                     Order Details
+-- *************************************************************************************************************************************
+-- Drop Table if already exists --
+DROP TABLE IF EXISTS order_details CASCADE;
+-- Creating orders table --
+CREATE TABLE order_details (
+    order_ID SERIAL,
+	product_ID SERIAL,
+	unit_Price NUMERIC,	
+	quantity INTEGER,	
+	discount NUMERIC,	
+	
+	FOREIGN KEY (order_ID) REFERENCES orders (order_ID),
+	FOREIGN KEY (product_ID) REFERENCES products (product_ID)
+	 
+);
+-- Inserting Data --
+\copy order_details FROM '/Users/mykolawauer/spiced_projects/GitHub/tahini-tensor-student-code/northwind_data_clean/data/order_details.csv' DELIMITER ',' CSV HEADER NULL 'NULL';
+
+
+
+	
+
